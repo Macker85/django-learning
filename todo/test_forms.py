ec652aa659b9b99c5a1f33aec299cc0e9e40d176
Macker85 <pmacker1985@gmail.com>
2023-03-30 20:12:04 +0000

    testing done

---
+++ todo/test_forms.py
@@ -0,0 +1,15 @@
+from django.test import TestCase
+from .forms import ItemForm
+
+
+class TestItemFrom(TestCase):
+
+    def test_item_name_is_required(self):
+        form = ItemForm({'name': ''})
+        self.assertFalse(form.is_valid())
+        self.assertIn('name', form.errors.keys())
+        self.assertEqual(form.errors['name'][0], 'This field is required.')
+
+    def test_done_field_is_not_required(self):
+        form = ItemForm({'name': 'Test ToDo Item'})
+        self.assertTrue(form.is_valid())
